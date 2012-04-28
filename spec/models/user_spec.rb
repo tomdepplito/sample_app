90040cbe733f4431a678fd864c49ca5d59c05f6b
tom <tomdepplito@gmail.com>
2012-04-27 21:32:32 -0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,7 +27,9 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) } 
   it { should respond_to(:authenticate) }
-
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
   it { should be_valid }
  
   describe "when email is not present" do
@@ -105,4 +107,9 @@ describe "return value of authenticate method" do
       specify { user_for_invalid_password.should be_false }
     end
   end 
+  
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
